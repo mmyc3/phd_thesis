9209f0a39509aab30caac19f5e30871cb49534d9
mmyc3 <33218646+mmyc3@users.noreply.github.com>
2022-05-08 22:10:12 +0100

    Update bedtools_sv_filter.sh

--- bedtools_sv_filter.sh
+++ bedtools_sv_filter.sh
@@ -7,42 +7,35 @@
 # INPUT:
         # 1: Tab-delimited list of chromosomal coordinates (regions.txt)
         # 2. A list of full-paths to VCF files of interest (vcf_list.txt)
+	
+#! /usr/bin/env bash
 
-#! usr/bin/env bash
-
-#BSUB -q medium
+#BSUB -q long
 #BSUB -P re_gecip_renal
 #BSUB -o sv_%J.stdout
 #BSUB -e sv_%J.stderr
-#BSUB -cwd /re_gecip/renal/mchan/PUV/data/
-#BSUB -R "rusage[mem=10000]"
+#BSUB -cwd /re_gecip/renal/mchan/CAKUT/SV/cakut
+#BSUB -R "rusage[mem=10000] span[hosts=1]"
 
 module load bio/BEDTools/2.27.1-foss-2018b
+module load bio/BCFtools/1.11-GCC-8.3.0
 
-VCFS="/re_gecip/renal/mchan/PUV/data/${COHORT}_vcf_list.txt"
-REGIONS="/re_gecip/renal/mchan/PUV/data/${GENE}.bed"
-OUTPATH="/re_gecip/renal/mchan/PUV/analysis/"
+COHORT=cakut_ancestry_matched_controls
+GENE=gencode
+
+VCFS="/re_gecip/renal/mchan/CAKUT/SV/data/cakut_control_vcf_list_v10.txt"
+EXONS="/re_gecip/renal/mchan/CAKUT/SV/data/gencode_exons_v29.bed"
+CRE="/re_gecip/renal/mchan/CAKUT/lists/encode_cre.txt"
+OUTPATH="/re_gecip/renal/mchan/CAKUT/SV/cakut/data"
 
 while read -r vcf; do
 	vfile="${vcf##*/}"
-        OUTFILE="${OUTPATH}/${vfile}.${COHORT}_${GENE}.vcf"
-	bedtools intersect -a ${vcf} \
-	-b ${REGIONS} \
-	-u \
-	-header > ${OUTFILE}
+        OUTFILE="${OUTPATH}/${vfile}.${GENE}.vcf"
+	bedtools intersect -a ${vcf} -b ${EXONS} -u -header | \
+	bcftools view -f PASS -o ${OUTFILE}
 done < ${VCFS}
 
-cd $OUTPATH
-ls *.${COHORT}_${GENE}.vcf > ${COHORT}_vcf.txt
+cd ${OUTPATH}
+ls *.${GENE}.vcf > ${COHORT}_${GENE}_vcf.txt
 
-while read -r vcf; do
-	vfile="${vcf##*/}"
-        OUTFILE="${OUTPATH}/${vfile}.filtered.vcf"
-	bcftools view ${vcf} \
-	-f PASS,. \
-	-o ${OUTFILE}
-done < ${COHORT}_vcf.txt
 
-rm *${COHORT}_${GENE}.vcf
-ls *.${COHORT}_${GENE}.vcf.filtered.vcf > ${COHORT}_vcf.txt
-sed -i 's/^/\/re_gecip\/renal\/mchan\/PUV\/analysis\//g' ${COHORT}_vcf.txt
